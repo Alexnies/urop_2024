003b56267c447506d1ef9ec50069ebca3b3cf42c
Alexnies <an1821@ic.ac.uk>
2024-07-23 11:50:29 +0100

    fixed gopython on ubuntu

--- gopython_test.py
+++ gopython_test.py
@@ -1,3 +1,16 @@
+import os
+
+os.environ['GPROMSHOME'] = '/usr/local/pse/gPROCESS_2023/gPROMS-core_2022.2.2.55277'
+
+# Add the directory to PYTHONPATH
+go_python_path = '/usr/local/pse/gPROCESS_2023/gPROMS-core_2022.2.2.55277/bin'
+
+if go_python_path not in os.getenv("PATH"):
+    os.environ["PATH"] += os.pathsep + go_python_path
+
+print("GPROMSHOME:", os.getenv("GPROMSHOME"))
+print("PATH:", os.getenv("PATH"))
+
 import gopython
 
 status = gopython.start_only()
